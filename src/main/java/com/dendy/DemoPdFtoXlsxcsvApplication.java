68ed8b5dc3248b557579a5462493741dd54351ca
dendy192 <dtiawan192@gmail.com>
2024-06-03 19:49:01 +0700

    first commit

---
+++ src/main/java/com/dendy/DemoPdFtoXlsxcsvApplication.java
@@ -0,0 +1,13 @@
+package com.dendy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPdFtoXlsxcsvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoPdFtoXlsxcsvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dendy/DemoPdFtoXlsxcsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dendy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPdFtoXlsxcsvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
